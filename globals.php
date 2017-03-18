45678e8f4405414df4074c12b2b4934786645379
Barnabas Forgo <Barnabas.Forgo@mhr.co.uk>
2017-03-18 17:03:30 +0000

    add config

--- globals.php
+++ globals.php
@@ -6,10 +6,10 @@ function DebugLog($log){
   $result = file_put_contents(__DIR__ . "/log/debug.log", date("c") . " - " . $log . "\n", FILE_APPEND);
 }
 
-$GLOBALS['CRONOFY_CLIENT_ID'] = "";
-$GLOBALS['CRONOFY_CLIENT_SECRET'] = "";
+$GLOBALS['CRONOFY_CLIENT_ID'] = $_ENV['ID'];
+$GLOBALS['CRONOFY_CLIENT_SECRET'] = $_ENV['SECRET'];
 
-$GLOBALS['DOMAIN'] = "";
+$GLOBALS['DOMAIN'] = $_ENV['URL'];
 
 if($GLOBALS['CRONOFY_CLIENT_ID'] == "" || $GLOBALS['CRONOFY_CLIENT_SECRET'] == "" || $GLOBALS['DOMAIN'] == ""){
   header('Location: /setup.php');
